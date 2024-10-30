5e4a0a6614d40b0b3045edffd7d7de7dd42d57f5
rbuler <rafalbuler@gmail.com>
2024-10-30 22:59:16 +0100

    Updated config and main

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import utils
 from radiomics import setVerbosity
 from dataset import CRCDataset
 from reduce_dim_features import reduce_dim
-from classifiers import Classifier
+# from classifiers import Classifier
 import pandas as pd
 from warnings import simplefilter
 simplefilter(action="ignore", category=pd.errors.PerformanceWarning)
@@ -39,32 +39,25 @@ if __name__ == '__main__':
                          save_new_masks=False)
 
     radiomic_features = dataset.radiomic_features
-    reduce_dim(radiomic_features, comparison_type='node') # 'colon', 'node', 'fat', 'all'
 
-    # Example classification using XGBoost
-    data = radiomic_features
-    filtered_data = data[data['class_label'].isin([2, 5])]
-    filtered_data['class_label'] = filtered_data['class_label'].map({2: 0, 5: 1})
-    y = filtered_data['class_label']
-    X = filtered_data.drop(columns=['class_label', 'patient_id', 'instance_label'])
+
+    ## x = reduce_dim  TODO add return value
+    reduce_dim(radiomic_features, comparison_type='node', scaler='standard') # 'colon', 'node', 'fat', 'all'
+    
+    ## Example classification using XGBoost
+    # data = radiomic_features
     # filtered_data = data[data['class_name'].isin(['lymph_node_positive', 'lymph_node_negative'])]
     # filtered_data['class_name'] = filtered_data['class_name'].map({'lymph_node_positive': 1, 'lymph_node_negative': 0})
     # y = filtered_data['class_name']
     # X = filtered_data.drop(columns=['class_label', 'patient_id', 'instance_label', 'class_name'])
-    patient_ids = filtered_data['patient_id']
-
-    X = X.values
-    y = y.values
-
-    classifier = Classifier(X, y, patient_ids.values, classifier_name='XGBoost')
-    classifier.train_classifier()
-
-
-# %%
-# try:
-#     view_slices(img, mask, title='3D Mask Slices')
-# except Exception as e:
-#     print(e)
-
-
+    # patient_ids = filtered_data['patient_id']
+    # X = X.values
+    # y = y.values
+    # classifier = Classifier(X, y, patient_ids.values, classifier_name='XGBoost')
+    # classifier.train_classifier()
+    
+    # try:
+    #     view_slices(img, mask, title='3D Mask Slices')
+    # except Exception as e:
+    #     print(e)
 # %%
