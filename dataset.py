fbff95627554e0ecbd485374307bbfc4726789c2
rbuler <rafalbuler@gmail.com>
2025-01-22 13:01:16 +0100

    Added TODO to address other possibilities for T overstaging, including ‘Tx’ and ‘Tis’ in future

--- dataset.py
+++ dataset.py
@@ -60,10 +60,17 @@ class CRCDataset(Dataset):
 
         self._clean_tnm_clinical_data()
         category_order_N = {'0': 0, '1a': 1, '1b': 2, '2a': 3, '2b': 4, '1c': np.nan, 'nan': np.nan}
-        category_order_T = {'0': 0, '1': 1, '2': 2, '3': 3, '4a': 4, '4b': 5, 'nan': np.nan}
-        self._compute_overstaging_tnm('wmT', 'pT', category_order_T, 'overstaging_T')
-        self._compute_overstaging_tnm('wmN', 'pN', category_order_N, 'overstaging_N')
-
+        self._compute_overstaging_tnm('wmN', 'pN', category_order_N, 'N_wm_p_overstaging')
+        
+        
+        ### TODO - think of 'x' and 'is' in the future
+        #         
+        # category_order_T = {'0': 0, '1': 1, '2': 2, '3': 3, '4a': 4, '4b': 5, 'nan': np.nan}
+        # self._compute_overstaging_tnm('wmT', 'pT', category_order_T, 'overstaging_T')
+        #
+        #
+        
+        
         mapping = {"background": 0,
             "colon_positive": 1,
             "lymph_node_positive": 2,
@@ -145,8 +152,8 @@ class CRCDataset(Dataset):
 
         self.clinical_data['TNM wg mnie'] = self.clinical_data['TNM wg mnie'].apply(make_lower_case)
     
-        self.clinical_data['wmT'] = self.clinical_data['TNM wg mnie'].str.extract(r'T(\d+[a-bA-B]?)')
-        self.clinical_data['wmN'] = self.clinical_data['TNM wg mnie'].str.extract(r'N(\d+[a-cA-C]?)')
+        self.clinical_data['wmT'] = self.clinical_data['TNM wg mnie'].str.extract(r'T(\d+[a-bx]?)')
+        self.clinical_data['wmN'] = self.clinical_data['TNM wg mnie'].str.extract(r'N(\d+[a-c]?)')
     
 
     def _compute_overstaging_tnm(self, wm_column: str, p_column: str, category_order: dict, result_column: str):
