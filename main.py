4077a10b6ff8f7b74e6e2410b298fd840f9f8f70
rbuler <rafalbuler@gmail.com>
2025-02-13 13:36:37 +0100

    Moved network and training-related code to a separate file

--- main.py
+++ main.py
@@ -11,8 +11,7 @@ from sklearn import linear_model
 from torch.utils.data import DataLoader
 from sklearn.model_selection import StratifiedKFold, StratifiedShuffleSplit
 from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import accuracy_score, precision_score
-from sklearn.metrics import recall_score, f1_score, confusion_matrix
+from network import MILNetwork, train_net, test_net
 from reduce_dim_features import icc_select_reproducible
 from reduce_dim_features import select_best_from_clusters
 from dataset import CRCDataset
@@ -21,133 +20,6 @@ from utils import generate_mil_bags, summarize_bags
 logger = logging.getLogger(__name__)
 logger_radiomics = logging.getLogger("radiomics")
 logging.basicConfig(level=logging.ERROR)
-
-class MILNetwork(nn.Module):
-    def __init__(self, input_dim, hidden_dim):
-        super(MILNetwork, self).__init__()
-        
-        self.instance_encoder = nn.Sequential(
-            nn.Linear(input_dim, hidden_dim),
-            nn.ReLU(),
-            nn.Linear(hidden_dim, hidden_dim),
-            nn.ReLU())
-        
-        self.aggregation = lambda x: torch.max(x, dim=1)[0]
-        self.classifier = nn.Sequential(nn.Linear(hidden_dim, 1), nn.Sigmoid())
-
-    def forward(self, bag):
-        b, n, feats = bag.size()  # batch size, number of instances, number of features
-        bag = bag.view(b * n, feats)
-        x = self.instance_encoder(bag)
-        x = x.view(b, n, -1)
-        x = self.aggregation(x)
-        x = self.classifier(x)
-        return x
-
-
-def train_net(model, criterion, optimizer, train_loader, valid_loader, num_epochs=10, patience=10):
-    history = {'epoch': [], 'train_acc': [], 'val_acc': [], 'train_loss': [], 'val_loss': []}
-    best_val_loss = float('inf')
-    patience_counter = 0
-    
-    for epoch in range(num_epochs):
-        corrects = 0
-        epoch_loss = 0
-        model.train()
-        for batch in train_loader:
-            for bag in batch:
-                optimizer.zero_grad()
-                instances = torch.stack(bag['instances']).unsqueeze(0)
-                bag_label = torch.tensor(bag['bag_label'], dtype=torch.float32)
-
-                output = model(instances)
-                loss = criterion(output[0], bag_label)
-                loss.backward()
-                optimizer.step()
-
-                epoch_loss += loss.item()
-                preds = (output > 0.5).float()
-                corrects += (preds == bag_label.item()).sum().item()
-
-        train_loss = epoch_loss / len(train_loader)
-        train_acc = corrects / len(train_loader)
-
-        # val loop
-        model.eval()
-        val_corrects = 0
-        val_loss = 0
-        with torch.no_grad():
-            for batch in valid_loader:
-                for bag in batch:
-                    instances = torch.stack(bag['instances']).unsqueeze(0)
-                    bag_label = torch.tensor(bag['bag_label'], dtype=torch.float32)
-
-                    output = model(instances)
-                    loss = criterion(output[0], bag_label)
-
-                    val_loss += loss.item()
-                    preds = (output > 0.5).float()
-                    val_corrects += (preds == bag_label.item()).sum().item()
-        
-        val_loss /= len(valid_loader)
-        val_acc = val_corrects / len(valid_loader)
-
-        history['epoch'].append(epoch + 1)
-        history['train_acc'].append(train_acc)
-        history['val_acc'].append(val_acc)
-        history['train_loss'].append(train_loss)
-        history['val_loss'].append(val_loss)
-
-        if val_loss < best_val_loss:
-            best_val_loss = val_loss
-            patience_counter = 0
-            # store state dict
-            # TODO
-            # return best model
-            # best_model_state = model.state_dict()
-        else:
-            patience_counter += 1
-
-        if patience_counter >= patience:
-            print(f"Early stopping at epoch {epoch + 1}")
-            break
-
-    return model, history
-
-
-def test_net(model, test_loader):
-    model.eval()
-    all_preds = []
-    all_labels = []
-
-    with torch.no_grad():
-        for batch in test_loader:
-            for bag in batch:
-                instances = torch.stack(bag['instances']).unsqueeze(0)
-                bag_label = bag['bag_label']
-
-                output = model(instances)
-                preds = (output > 0.5).float().item()
-                
-                all_preds.append(preds)
-                all_labels.append(bag_label.item())
-
-    all_preds = np.array(all_preds)
-    all_labels = np.array(all_labels)
-
-    metrics = {
-        "accuracy": accuracy_score(all_labels, all_preds),
-        "precision": precision_score(all_labels, all_preds, zero_division=0),
-        "recall": recall_score(all_labels, all_preds, zero_division=0),
-        "f1_score": f1_score(all_labels, all_preds, zero_division=0),
-    }
-
-    conf_matrix = confusion_matrix(all_labels, all_preds)
-
-
-    return metrics, conf_matrix
-
-
 
 
 # MAKE PARSER AND LOAD PARAMS FROM CONFIG FILE--------------------------------
@@ -265,8 +137,8 @@ if __name__ == '__main__':
 
     bag_ids = [bag['patient_id'] for bag in bags]
     bag_labels = [bag['bag_label'].item() for bag in bags]
-    skf = StratifiedKFold(n_splits=5, shuffle=True, random_state=seed)
     # %%
+    skf = StratifiedKFold(n_splits=5, shuffle=True, random_state=seed)
     all_histories = []
     all_metrics = []
     all_conf_matrices = []
