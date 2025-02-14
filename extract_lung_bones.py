fa81817da6288fb670d923b9c10932e750f57ba6
rbuler <rafalbuler@gmail.com>
2025-02-14 16:12:32 +0100

    Added check to ensure mask region is not cut and implemented saving after cropping

--- extract_lung_bones.py
+++ extract_lung_bones.py
@@ -1,4 +1,5 @@
 # %%
+import os
 import numpy as np
 import nibabel as nib
 import matplotlib.pyplot as plt
@@ -24,7 +25,7 @@ def segment_lung_and_bone(image_hu, lung_hu_range=(-1000, -500), bone_hu_range=(
     return lung_mask, bone_mask
 
 
-def process_images(image_paths, window_size=10):
+def process_images(image_paths, mask_paths, window_size=10):
     """
     Process the given images to segment lung and bone regions, calculate statistics, and return summed values.
 
@@ -59,18 +60,29 @@ def process_images(image_paths, window_size=10):
 
         lung_end_index = max_descend_index * window_size
 
-
         """
         TODO - Implement a more robust method to find the end of the lung region.
         AND also implement a method to find the start of the bone (lower pelvis) region.
         """
 
-        
+        # how many nonzero pixels are in the mask slice
+        nifti_mask = nib.load(mask_paths[idx])
+        mask = nifti_mask.get_fdata()
+        mask = np.squeeze(mask) if len(mask.shape) == 4 else mask
+        mask = mask > 0
+        mask_slices_sum = np.sum(mask, axis=(0, 1))[::-1]
+
+        # normalize lung_slices_sum and mask_slices_sum
+        lung_slices_sum = lung_slices_sum / np.max(lung_slices_sum)
+        mask_slices_sum = mask_slices_sum / np.max(mask_slices_sum)
+
         results.append({
             "image_index": idx + 1,
             "lung_slices_sum": lung_slices_sum,
             "bone_slices_sum": bone_slices_sum,
-            "lung_end_index": lung_end_index  # Index of the largest descend in means
+            "lung_end_index": lung_end_index,  # largest descend in means
+            "mask_slices_sum": mask_slices_sum,
+            "mask_first_nonzero": np.argmax(mask_slices_sum > 0)
         })
 
     return results
@@ -79,34 +91,56 @@ def process_images(image_paths, window_size=10):
 # Example usage
 if __name__ == '__main__':
 
-    image_paths = [
-        '/media/dysk_a/jr_buler/RJG-gumed/RJG-6_labels_version/8a/8 (M, 2023-11-22).nii.gz',
-        '/media/dysk_a/jr_buler/RJG-gumed/RJG-6_labels_version/75a/75 (2024-03-21).nii.gz'
-    ]
+    nii_pth = "/media/dysk_a/jr_buler/RJG-gumed/RJG_13-02-25_nii_labels"
+    image_paths = []
+    mask_paths = []
 
-    results = process_images(image_paths, window_size=5)
+    for root, dirs, files in os.walk(nii_pth, topdown=False):
+        for name in files:
+            f = os.path.join(root, name)
+            if 'labels.nii.gz' in f:
+                mask_paths.append(f)
+            elif 'instance_mask.nii.gz' in f:
+                continue
+            elif 'nii.gz' in f:
+                image_paths.append(f)
+            elif 'mapping.pkl' in f:
+                continue
 
-    plt.figure(figsize=(12, 6))
-    plt.subplot(1, 2, 1)
-    plt.plot(results[0]["bone_slices_sum"], label='Bone Slices Sum - Image 1')
-    plt.plot(results[1]["bone_slices_sum"], label='Bone Slices Sum - Image 2')
-    plt.legend()
-    plt.title('Bone Slices Sum Comparison')
-    plt.xlabel('Slice Index')
-    plt.ylabel('Sum of Bone Pixels')
+    image_paths = image_paths[:10]
+    results = process_images(image_paths, mask_paths, window_size=20)
+    
+    slice_indexes_to_cut = [min(result["lung_end_index"], result["mask_first_nonzero"]) for result in results] # index for each patient
 
-    plt.subplot(1, 2, 2)
-    plt.plot(results[0]["lung_slices_sum"], label='Lung Slices Sum - Image 1')
-    plt.plot(results[1]["lung_slices_sum"], label='Lung Slices Sum - Image 2')
-    plt.axvline(x=results[0]["lung_end_index"], color='r', linestyle='--', label='Lung End Index - Image 1')
-    plt.axvline(x=results[1]["lung_end_index"], color='g', linestyle='--', label='Lung End Index - Image 2')
-    plt.legend()
-    plt.title('Lung Slices Sum Comparison')
-    plt.xlabel('Slice Index')
-    plt.ylabel('Sum of Lung Pixels')
-    plt.tight_layout()
-    plt.show()
+    # save new images and masks after cutting the slices
+    # so image now is slice[slice_index_to_cut:]
+    # and mask is mask[slice_index_to_cut:]
+    for idx, image_path in enumerate(image_paths):
+        nifti_image = nib.load(image_path)
+        image_hu = nifti_image.get_fdata()
+        image_hu = np.squeeze(image_hu) if len(image_hu.shape) == 4 else image_hu
+        nifti_mask = nib.load(mask_paths[idx])
+        mask = nifti_mask.get_fdata()
+        mask = np.squeeze(mask) if len(mask.shape) == 4 else mask
+        image_hu = image_hu[:, :, slice_indexes_to_cut[idx]:]
+        mask = mask[:, :, slice_indexes_to_cut[idx]:]
 
-    print("Lung End Index - Image 1:", results[0]["lung_end_index"])
-    print("Lung End Index - Image 2:", results[1]["lung_end_index"])
+        new_image_path = image_path.replace(".nii.gz", "_cut.nii.gz")
+        new_mask_path = mask_paths[idx].replace(".nii.gz", "_cut.nii.gz")
+        nib.save(nib.Nifti1Image(image_hu, nifti_image.affine), new_image_path)
+        nib.save(nib.Nifti1Image(mask, nifti_mask.affine), new_mask_path)
+    
+    draw = False
+    if draw:
+        for result in results:
+            plt.figure(figsize=(12, 6))
+            plt.plot(result["lung_slices_sum"], label='Lung Slices Sum')
+            plt.plot(result["mask_slices_sum"], label='Mask Slices Sum', linestyle='--')
+            plt.axvline(x=result["lung_end_index"], color='r', linestyle='-', label='Lung End Index')
+            plt.legend()
+            plt.title('Lung and Mask Slices Sum')
+            plt.xlabel('Slice Index')
+            plt.ylabel('Sum of Pixels')
+            plt.tight_layout()
+            plt.show()
 # %%
