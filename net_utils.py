657af2708ae179dac67082d18390ae3ebf975c58
rbuler <rafalbuler@gmail.com>
2025-04-14 13:06:15 +0200

    Minor fixes

--- net_utils.py
+++ net_utils.py
@@ -36,8 +36,12 @@ def train_net(mode, root, model, criterion, optimizer, dataloaders, num_epochs=1
 
             elif mode == '3d':
                 inputs, targets = img_patch.to(device, dtype=torch.float32), mask_patch.to(device, dtype=torch.long)
-                inputs = inputs.permute(1, 0, 2, 3, 4)
-                targets = targets.permute(1, 0, 2, 3, 4)
+                if inputs.shape[1] == 1 and len(inputs.shape) == 5:
+                    inputs = inputs.permute(1, 0, 2, 3, 4)
+                    targets = targets.permute(1, 0, 2, 3, 4)
+                elif inputs.shape[1] != 1 and len(inputs.shape) == 5:
+                    inputs = inputs.reshape(inputs.shape[0] * inputs.shape[1], *inputs.shape[2:]).unsqueeze(1)
+                    targets = targets.reshape(targets.shape[0] * targets.shape[1], *targets.shape[2:]).unsqueeze(1)
         # outputs, logits = model(img_patch, return_logits=True)
 
             optimizer.zero_grad()
